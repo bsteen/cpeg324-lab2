16bfffc2c8b94abeaf6278b038b70e588fb767d9
bsteen <steenkamerben@gmail.com>
2017-04-01 18:17:17 -0400

    Added ghdl.

--- 4_bit_add_sub.vhdl
+++ 4_bit_add_sub.vhdl
@@ -1,11 +1,35 @@
+-- Benjamin Steenkamer and Abraham McIlvaine
+-- CPEG 324-010
+-- Lab 2: VHDL Componetns - gvhdl 4_bit_add_sub.vhdl
+-- 4/X/17
+
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity addsub is
-    port(ina, inb : in std_logic; op : out std_logic);
-end entity addsub;
+entity half_adder is
+port(a, b : in std_logic;
+    sum, carry : out std_logic);
+end entity half_adder;
 
-architecture behavioral of addsub is
+architecture behavioral of half_adder is
 begin
-    op <= ina and inb;
-end architecture;
+sum <= a xor b;
+carry <= a and b;
+end architecture behavioral;
+
+-- entity full_adder is
+--
+-- end entity full_adder;
+--
+-- entity addsub_4bit is
+--     port(input_a : in std_logic_vector(3 downto 0);
+--          input_b : in std_logic_vector(3 downto 0);
+--          sum : out std_logic_vector(3 downto 0);
+--          overflow: out std_logic;
+--          underflow: out std_logic);
+-- end entity addsub_4bit;
+--
+-- architecture behavioral of addsub_4bit is
+-- begin
+--
+-- end architecture behavioral;
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # cpeg324-lab2
 CPEG 324 - Lab 2 (VHDL Components)
+
+See lab2.pdf for details.
